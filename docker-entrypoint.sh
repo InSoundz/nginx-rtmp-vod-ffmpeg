3fd4f83310fa797a29a086862dde11dec377d3b7
matansag <matan@insoundz.com>
2017-05-23 10:34:14 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 nginx -g "daemon off;" &
 
-ffmpeg -f video4linux2 -i /dev/video0 -c:v libx264 -an -tune zerolatency -f flv rtmp://127.0.0.1:1935/webcam/video0
+ffmpeg -input_format mjpeg -fpsprobesize 10 -fflags +genpts+igndts -f v4l2 -i /dev/video0 -c:v libx264 -crf 30 -preset:v ultrafast -bf 2 -qp 18 -an -tune zerolatency -f flv rtmp://127.0.0.1:1935/webcam/video0
